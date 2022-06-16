37ebebc71b8e10099461f17a1a57276cc92d0e32
unknown <240781orT>
2022-06-16 00:52:51 -0500

    version 0.1

---
+++ routes/veterinarioRoutes.js
@@ -0,0 +1,35 @@
+import express from 'express';
+const router =  express.Router();
+import { registrar, perfil, confirmar, autenticar, olvidepasword, comprobarToken, nuevoPassword, 
+    back, restore, impotaDatos,exportD, actualizarPeril, actualizarPass  } from '../controllers/veterinarioController.js'
+import chekAuth from '../middleware/authMiddleware.js'
+
+
+//
+router.post('/', registrar)
+
+router.get('/confirmar/:t', confirmar)
+
+router.post("/login", autenticar )
+
+router.post("/olvide-password", olvidepasword)
+
+router.get("/olvide-password/:t", comprobarToken)
+router.post("/olvide-password/:t", nuevoPassword)
+
+//router.route("/olvide-password/:t").get(comprobarToken).post(nuevoPassword)
+
+///
+router.get('/perfil', chekAuth , perfil)
+router.put("/perfil/:id", chekAuth, actualizarPeril)
+
+router.post('/back', back)
+
+router.post("/restore", restore)
+router.post("/exportar", exportD)
+router.post("/importacion", impotaDatos)
+
+router.put("/actualizarPassword", chekAuth, actualizarPass)
+
+
+export default router;
